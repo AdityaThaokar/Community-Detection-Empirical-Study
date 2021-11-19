c238e551f838b12d444fdcc56933b961cdc2feac
AdityaThaokar <30742223+AdityaThaokar@users.noreply.github.com>
2021-11-19 22:38:15 +0530

    q1 updated with comments

--- q1.py
+++ q1.py
@@ -1,6 +1,10 @@
 import networkx as nx
-dolphin = nx.read_gml("C:\\Users\Aditya\\Desktop\\dolphins.gml")
+
 print("-----------Dolphins Statistics -----------")
+#reading the dolphin gml
+dolphin = nx.read_gml("C:\\Users\Aditya\\Desktop\\dolphins.gml")
+
+#getting the info about no. of nodes and edges
 print(nx.info(dolphin))
 print("Avg path length : ", nx.average_shortest_path_length(dolphin))
 average = nx.average_clustering(dolphin)
@@ -10,7 +14,10 @@ print("-----------------------------------------")
 
 
 print("-----------Karate Club Statistics -----------")
+#reading the karate gml
 karate = nx.read_gml("C:\\Users\Aditya\\Desktop\\karate.gml", label='id')
+
+#getting the info about no. of nodes and edges
 print(nx.info(karate))
 print("Avg path length : ", nx.average_shortest_path_length(karate))
 average = nx.average_clustering(karate)
@@ -18,8 +25,12 @@ print("Avg clustering coefficient : ", average)
 print("-----------------------------------------")
 
 
+
 print("-----------Jazz Statistics -----------")
+#reading the Jazz gml
 jazz = nx.read_weighted_edgelist("C:\\Users\Aditya\\Desktop\\jazz.net")
+
+#getting the info about no. of nodes and edges
 print(nx.info(jazz))
 print("Avg path length : ", nx.average_shortest_path_length(jazz))
 average = nx.average_clustering(jazz)
